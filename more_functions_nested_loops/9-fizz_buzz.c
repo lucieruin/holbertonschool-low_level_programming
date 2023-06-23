6dd73acf437837c149884afe54d28efd09af28dc
Lucieruin <Lucie.wattez@hotmail.fr>
2023-06-23 07:35:58 -0700

    prints numbers 1 to 100 with Fiss for multiples of 3 and Buzz for multiples of 5

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - prints numbers 10 to 100 , with Fizz for multi 3 and Buzz for multi 5
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	return (0);
+}
