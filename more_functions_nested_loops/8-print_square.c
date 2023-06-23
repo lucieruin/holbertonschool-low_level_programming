1370780202f1c82af241b56a301f2baeb9b105f1
Lucieruin <Lucie.wattez@hotmail.fr>
2023-06-23 06:00:13 -0700

    prints a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * orint_square - prints a square
+ * @size: is a number
+ *
+ */
+
+void print_square(int size)
+{
+	int row;
+	int col;
+
+	if (size > 0)
+	{
+		for (col = 1; col <= size; col++)
+		{
+			for (row = 1; row <= size; row++)
+			{
+				_putchar ('#');
+			}
+			_putchar ('\n');
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
+
