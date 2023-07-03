f8cb5ed04ad4a6e9f5e55369a0808a5cf29694b7
Lucieruin <Lucie.wattez@hotmail.fr>
2023-07-03 01:58:20 -0700

    function that prints a string followed by a new line

---
+++ recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/*library*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/*prototype*/
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+
+
+
+
+#endif
