cb4895dadbb8ec933335e30588e7ae0ffb7d1313
Lucieruin <Lucie.wattez@hotmail.fr>
2023-07-04 04:42:22 -0700

    program that prints its name, followed by a new line

---
+++ argc_argv/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/*library*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/*prototype*/
+int _putchar(char c);
+int main(int argc, char *argv[]);
+
+
+
+
+
+#endif
