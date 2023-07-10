2386f1143009d756b1164eb8ad099388635b14e9
Lucieruin <Lucie.wattez@hotmail.fr>
2023-07-10 02:14:28 -0700

    readme

---
+++ malloc_free/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/*library*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+/*prototype*/
+int _putchar(char c);
+
+
+
+
+
+
+#endif
