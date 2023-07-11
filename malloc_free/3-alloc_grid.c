03e0937787dbe2b205f486c818b063ca7dbdf3b1
Lucieruin <Lucie.wattez@hotmail.fr>
2023-07-11 05:11:37 -0700

    define * to int for output

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ar = malloc(height * sizeof(int));
+	ar = malloc(height * sizeof(int *));
 	if (ar == NULL)
 		return (NULL);
 
