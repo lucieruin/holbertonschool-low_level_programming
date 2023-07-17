9f8ce8e94335dfabcf492dc6f0333dd8c0de4a6e
Lucieruin <Lucie.wattez@hotmail.fr>
2023-07-17 02:11:23 -0700

    define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/*library*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/*structure*/
+
+/**
+ * struct dog - struct for dogs
+ * @name: first argument
+ * @age: second argument
+ * @owner: third argument
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/*prototype*/
+
+
+
+
+#endif
