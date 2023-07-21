8cc4821f7b4dbe7a38e63980b5050eeb689bcf9a
Lucieruin <Lucie.wattez@hotmail.fr>
2023-07-21 08:02:23 -0700

    a function that prints anything and update variadic_functions.h:

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,7 +4,18 @@
 /* library */
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
+/**
+ * struct print - print
+ * @type: The operator
+ * @f: The function associated
+ */
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list ap);
+} print_type;
 
 
 /* prototypes*/
@@ -12,6 +23,7 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 
 
