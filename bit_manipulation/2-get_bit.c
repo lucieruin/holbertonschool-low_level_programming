cdb6e5322f750d0247f2ea502f858287a1610857
Lucieruin <Lucie.wattez@hotmail.fr>
2023-08-04 01:48:35 -0700

    a function that returns the value of a bit at a given index and update main.h

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: integer
+ * @index: index
+ *
+ * Return: the value ofthe bit at index  or -1 if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= 32)
+		return (-1);
+	return ((n >> index) & 1);
+}
