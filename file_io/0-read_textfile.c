fdf067a1924dbcc17a94f8557b265356192cfc31
Lucieruin <Lucie.wattez@hotmail.fr>
2023-08-07 05:30:02 -0700

    a function that reads a text file and prints it to the POSIX standard output and add main.h

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the posix standard output
+ * @filename: a character
+ * @letters: len
+ *
+ * Return:
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, readFile, writeFile;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file < 0)
+		return (0);
+
+	buffer = malloc((letters) * sizeof(char));
+
+	if (buffer == NULL)
+	{
+		close(file);
+		return (0);
+	}
+	readFile = read(file, buffer, letters);
+	if (readFile < 0)
+	{
+		free(buffer);
+		close(file);
+		return (0);
+	}
+	writeFile = write(STDOUT_FILENO, buffer, readFile);
+	if (writeFile < 0 || writeFile != readFile)
+	{
+		free(buffer);
+		close(file);
+		return (0);
+	}
+	free(buffer);
+	close(file);
+	return (writeFile);
+}
---
+++ file_io/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+/* Library*/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+
+/* Prototypes*/
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+
+
+
+
+
+#endif
